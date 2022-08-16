70754f55767f98b47ffa1d5dd2c547530f8ca2de
Jacob McSwain <jacob.a.mcswain@gmail.com>
2022-08-16 00:41:21 -0500

    Fixup lint

--- src/App.vue
+++ src/App.vue
@@ -1,16 +1,16 @@
 <template>
   <div id="app">
-    <Header />
+    <SiteHeader />
     <router-view />
   </div>
 </template>
 
 <script>
-import Header from "./components/Header";
+import SiteHeader from "./components/SiteHeader";
 
 export default {
   components: {
-    Header,
+    SiteHeader,
   },
   methods: {
     supportsWebp: async function() {
