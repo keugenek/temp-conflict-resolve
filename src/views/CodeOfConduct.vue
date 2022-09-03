51de15d8a4c912f230fa41af93caa61e74da8290
Jacob McSwain <jacob.a.mcswain@gmail.com>
2022-09-02 21:32:30 -0500

    Add code of conduct

--- src/views/CodeOfConduct.vue
+++ src/views/CodeOfConduct.vue
@@ -4,7 +4,32 @@
       <mu-card>
         <mu-card-title title="Code of Conduct"></mu-card-title>
         <mu-card-text>
-          <h2>Don't be a dick</h2>
+          <h2>Summed up: "Don't be a dick"</h2>
+          <ul>
+            <li>Be welcoming to everyone.</li>
+            <li>Be considerate.</li>
+            <li>Be respectful.</li>
+            <li>Be mindful of your surroundings and of your fellow guests.</li>
+            <li>Sexist, racist, or otherwise discriminatory jokes and language are not allowed.</li>
+            <li>No personal insults, especially those using racist or sexist terms.</li>
+            <li>Do not harass others. If someone asks you to stop, then stop.</li>
+            <li>Any physical violence or intimidation, threatened or acted on, is a crime.</li>
+            <li>Respect and use preferred pronouns. This is a zero-tolerance rule.</li>
+            <li>Offensive comments related to gender or gender identity are not allowed.</li>
+            <li>You can record or take pictures, but please refrain from posting any on social media
+              until after the wedding ceremony.</li>
+            <li>Do not harass or otherwise impede the photographer.</li>
+          </ul>
+          <p>Violations of these rules will result in being asked to leave.
+            Failure to leave is considered trespass.</p>
+          <p>Disagreements happen, but are not an excuse for poor behavior and bad manners.
+            When disagreements do happen, work together to solve them effectively in a way that
+            ensures everyone's day is wonderful.</p>
+          <p>Not everyone will understand jokes, sarcasm, oblique references, colloquialisms, or
+            euphemisms in the same way you do. Always empathize with the other parties and recognize
+            they may come from a different background and interpret words differently than you.</p>
+          <p>Sexist, racist, or other prejudicial or exclusionary comments are never welcome at this
+            wedding. Violent threats or language directed against another person are similarly disallowed.</p>
         </mu-card-text>
       </mu-card>
     </mu-flex>
