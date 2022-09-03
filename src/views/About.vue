fe914bc7bbdc3433a0dd0d1fe27147d768934b03
Jacob McSwain <jacob.a.mcswain@gmail.com>
2022-09-02 21:33:33 -0500

    Add About Us content

--- src/views/About.vue
+++ src/views/About.vue
@@ -2,9 +2,55 @@
   <div style="width: 100%">
     <mu-flex justify-content="center">
       <mu-card>
-        <mu-card-title title="About Us"></mu-card-title>
+        <mu-card-title title="How We Met"></mu-card-title>
         <mu-card-text>
-          <h2>Coming Soon</h2>
+          <h2>Katelyn</h2>
+          <p>
+            &nbsp;&nbsp;&nbsp;&nbsp;
+            &nbsp;&nbsp;&nbsp;&nbsp;
+            &nbsp;&nbsp;&nbsp;&nbsp;
+            Jacob and I have been together for 8 going on 9 years.
+            We first met at Braum's about 10 years ago. At the time,
+            I didn't know he liked me. He was a very shy boy at the
+            time, so we didn't talk much at first. A friend and I made
+            jokes about how cute he was before I knew he liked me.
+            It was only after a coworker of ours had overheard him say
+            it that I found out. He was turning 16 and I had just turned
+            18 when he first asked me out on a date. I said no at first
+            due to the age gap, but he didn't give up and I eventually
+            said yes. We hit it off from there: we talked all the time,
+            and texted just as much. He bought me a promise ring about 4
+            years after we started dating and 6 years in he asked me to
+            marry him while we were driving home from my mothers house one
+            day. When we moved in together, I had 3 cats: Lazy, Lux, and
+            Nox. At first, he didn't want any more animals. That was, until
+            my mom brought home a foster that she had planned to adopt, but
+            we took her in instead. He fell in love with her, so now we have
+            a 4th cat named Storm. It has now been almost 9 years and we
+            have been together through thick and thin.
+          </p>
+          <br />
+          <h2>Jacob</h2>
+          <p>
+            Coming Soon
+          </p>
+        </mu-card-text>
+      </mu-card>
+    </mu-flex>
+    <br />
+    <mu-flex justify-content="center">
+      <mu-card>
+        <mu-card-title title="Favorite Memory"></mu-card-title>
+        <mu-card-text>
+          <h2>Katelyn</h2>
+          <p>
+            Coming Soon
+          </p>
+          <br />
+          <h2>Jacob</h2>
+          <p>
+            Coming Soon
+          </p>
         </mu-card-text>
       </mu-card>
     </mu-flex>
